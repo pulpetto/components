7d1f8f9b5e1f59e1eb03edc8e36ac8285518fbea
pulpetto <116948697+pulpetto@users.noreply.github.com>
2023-06-21 08:09:11 +0200

    generating component inside module

--- src/app/elements/elements.module.ts
+++ src/app/elements/elements.module.ts
@@ -2,13 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ElementsRoutingModule } from './elements-routing.module';
-
+import { ElementsHomeComponent } from './elements-home/elements-home.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    ElementsRoutingModule
-  ]
+    declarations: [ElementsHomeComponent],
+    imports: [CommonModule, ElementsRoutingModule],
 })
-export class ElementsModule { }
+export class ElementsModule {}
