ef300faf79b68fb24fdd38a8caa301df1824547b
pulpetto <116948697+pulpetto@users.noreply.github.com>
2023-06-22 22:24:04 +0200

    lazy loading (collections)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { CollectionsModule } from './collections/collections.module';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
     declarations: [AppComponent, HomeComponent, NotFoundComponent],
-    imports: [BrowserModule, CollectionsModule, AppRoutingModule],
+    imports: [BrowserModule, AppRoutingModule],
     providers: [],
     bootstrap: [AppComponent],
 })
