c9e15f070011891d3c8e580aa45b9310bb22eb9d
pulpetto <116948697+pulpetto@users.noreply.github.com>
2023-06-27 19:19:33 +0200

    communication from parent to child

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-collections-home',
-  templateUrl: './collections-home.component.html',
-  styleUrls: ['./collections-home.component.css']
+    selector: 'app-collections-home',
+    templateUrl: './collections-home.component.html',
+    styleUrls: ['./collections-home.component.css'],
 })
 export class CollectionsHomeComponent {
+    data = [
+        { name: 'James', age: 24, job: 'designer' },
+        { name: 'Bob', age: 46, job: 'engineer' },
+        { name: 'Alice', age: 26, job: 'engineer' },
+    ];
 
+    headers = [
+        { key: 'name', label: 'Name' },
+        { key: 'age', label: 'Age' },
+        { key: 'job', label: 'Job' },
+    ];
 }
