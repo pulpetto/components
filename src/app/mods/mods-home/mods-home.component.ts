6eab13f99612adcf3b169276b4487810a6d65c93
pulpetto <116948697+pulpetto@users.noreply.github.com>
2023-06-29 22:28:11 +0200

    accordion done | Project finished

--- src/app/mods/mods-home/mods-home.component.ts
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -7,6 +7,20 @@ import { Component } from '@angular/core';
 })
 export class ModsHomeComponent {
     modalOpen = false;
+    items = [
+        {
+            title: 'Why i the sky blue',
+            content: 'fwnfiuw fwufnwf wifwaf aifwa',
+        },
+        {
+            title: 'What does an orange taste like',
+            content: 'fwfw fwf agw gaw wgwaga',
+        },
+        {
+            title: 'What color is that cat',
+            content: 'wfwf wf wf w fw fwwwwwwwww',
+        },
+    ];
 
     onClick() {
         this.modalOpen = !this.modalOpen;
